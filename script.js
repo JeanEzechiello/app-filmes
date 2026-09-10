c903d7e1baf8be1a343132132a5ff5bf9e44e5f7
Jean Ezechiello <jeanezechielo2008@gmail.com>
2026-09-09 22:32:41 -0300

    fix: corrige duplicação de favoritos e sincroniza visual da lista

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let ipt = document.getElementById('iptfilme')
 let button = document.getElementById('searchbutton')
 let res = document.getElementById('res')
+let verFavoritos = document.getElementById('verFavoritos')
 let dados
 let favoritos = JSON.parse(localStorage.getItem('favoritos')) || []
 
@@ -35,12 +36,12 @@ ipt.addEventListener('keydown', function(event) {
 
 res.addEventListener('click', function(event) {
     if (event.target.dataset.id) {
-        let filmeFavoritado = dados.results.find(filme => filme.id == event.target.dataset.id)
-
-        if (event.target.classList.contains('favoritado')) {
+        if (favoritos.some(filme => filme.id == event.target.dataset.id))  {
             favoritos = favoritos.filter(filme => filme.id != event.target.dataset.id)
             event.target.classList.remove('favoritado')
+            event.target.parentElement.remove()
         } else {
+            let filmeFavoritado = dados.results.find(filme => filme.id == event.target.dataset.id)
             favoritos.push(filmeFavoritado)
             event.target.classList.add('favoritado')
         }
@@ -49,4 +50,19 @@ res.addEventListener('click', function(event) {
     }
 })
 
+verFavoritos.addEventListener('click', function() {
+    mostrarFavoritos()
+})
+
+function mostrarFavoritos() {
+    res.innerHTML = ''
+    favoritos.forEach(filme => {
+        res.innerHTML += `<div class = "card-filme"> 
+        <img src = "https://image.tmdb.org/t/p/w200${filme.poster_path}" alt= "${filme.title}">
+        <h3>${filme.title}</h3>
+        <p>Nota: ${filme.vote_average}</p>
+        <button class="btn-favoritar favoritado" data-id="${filme.id}">★</button>
+    </div>`
+    })
+}
 
